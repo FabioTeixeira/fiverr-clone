f3e0077fa0b4a2fc13b460854c1da45175cddef7
Fábio Teixeira <fabioteixeira@Fabios-MacBook-Pro.local>
2024-06-03 21:39:42 +0200

    add create gig page

---
+++ app/seller/[username]/manage-gigs/create/page.tsx
@@ -0,0 +1,13 @@
+import {CreateForm} from '@/components/create-form'
+
+interface CreateGigProps {
+    params: {
+        username: string
+    }
+}
+
+const CreateGig = () => {
+
+}
+
+export default CreateGig;
