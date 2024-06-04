12672f85129ff313dc526357a5162cca8351de64
FabioTeixeira <fabio.mf.teixeira@gmail.com>
2024-06-04 20:56:02 +0200

    Added ui components

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 
 export const Loading = () => {
-    return (
-        <div className="h-full w-full flex justify-center-items">
-            <Image src="/logo.svg"
-                alt="Loading" 
-                width={120} 
-                height={120}
-                className="animate-pulse duration-700" />
-        </div>
-    )
-}
+  return (
+    <div className="h-full w-full flex justify-center-items">
+      <Image
+        src="/logo.svg"
+        alt="Loading"
+        width={120}
+        height={120}
+        className="animate-pulse duration-700"
+      />
+    </div>
+  );
+};
