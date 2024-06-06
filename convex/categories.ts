d53f9cb1d95873ddbfa5bb60ce966d76116bb000
FabioTeixeira <fabio.mf.teixeira@gmail.com>
2024-06-06 21:25:49 +0200

    fix categories

--- convex/categories.ts
+++ convex/categories.ts
@@ -3,13 +3,10 @@ import { query } from "./_generated/server";
 import { Id } from "./_generated/dataModel";
 
 export const get = query({
-  args: {
-    id: v.id("gigs"),
-  },
-  handler: async (ctx, args) => {
-    const categorires = await ctx.db.query("categories").collect();
+  handler: async (ctx) => {
+    const categories = await ctx.db.query("categories").collect();
 
-    const categoriesWithSubcategoriesRelations = categorires.map((category) => {
+    const categoriesWithSubcategoriesRelations = categories.map((category) => {
       return ctx.db
         .query("subcategories")
         .withIndex("by_category", (q) => q.eq("categoryId", category._id))
@@ -21,5 +18,11 @@ export const get = query({
           };
         });
     });
+
+    const categoriesWithSubcategories = await Promise.all(
+      categoriesWithSubcategoriesRelations
+    );
+
+    return categoriesWithSubcategories;
   },
 });
